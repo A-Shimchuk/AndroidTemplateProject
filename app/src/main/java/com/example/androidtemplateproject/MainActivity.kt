7a7e690a35b09f0f4ddda545ffbf61b070da2d27
Alexey Shimchuk <a.shimchuk@vkteam.ru>
2026-03-10 14:08:39 +0300

    Kotlin-base-and-app-structure - Second task completed

--- app/src/main/java/com/example/androidtemplateproject/MainActivity.kt
+++ app/src/main/java/com/example/androidtemplateproject/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.androidtemplateproject
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androidtemplateproject.MessageReviewActivity.MessageReviewActivity
 import com.example.androidtemplateproject.ui.theme.AndroidTemplateProjectTheme
@@ -42,6 +47,15 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        Button(
+                            modifier = Modifier.size(height = 60.dp, width = 240.dp),
+                            onClick = { call(context, textFieldState.value) }
+                        ) {
+                            Text("Позвонить другу")
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
                         TextField(
                             value = textFieldState.value,
                             onValueChange = { textFieldState.value = it },
@@ -56,6 +70,8 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text("Открыть вторую Activity")
                         }
+
+
                     }
                 }
             }
@@ -65,6 +81,16 @@ class MainActivity : ComponentActivity() {
     fun routeToTextReviewActivity(context: Context, text: String) {
         val intent = Intent(context, MessageReviewActivity::class.java).apply {
             putExtra("text", text)
+        }
+
+        context.startActivity(intent)
+    }
+    
+    fun call(context: Context, phone: String) {
+        // TODO: - Здесь валидацию на номер можно
+
+        var intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phone")
         }
 
         context.startActivity(intent)
