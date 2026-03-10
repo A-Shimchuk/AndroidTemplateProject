d9af91943045b05e7a1ef332b2d5b3c6fbe83788
Alexey Shimchuk <a.shimchuk@vkteam.ru>
2026-03-10 13:24:04 +0300

    Kotlin-base-and-app-structure - First task completed

--- app/src/main/java/com/example/androidtemplateproject/MainActivity.kt
+++ app/src/main/java/com/example/androidtemplateproject/MainActivity.kt
@@ -1,47 +1,72 @@
 package com.example.androidtemplateproject
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.androidtemplateproject.MessageReviewActivity.MessageReviewActivity
 import com.example.androidtemplateproject.ui.theme.AndroidTemplateProjectTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidTemplateProjectTheme {
+                val context = LocalContext.current
+                // Создание рективного состояния в Compose
+                val textFieldState = remember { mutableStateOf("") }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        TextField(
+                            value = textFieldState.value,
+                            onValueChange = { textFieldState.value = it },
+                            label = { Text("Введите текст") }
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Button(
+                            modifier = Modifier.size(height = 60.dp, width = 240.dp),
+                            onClick = { routeToTextReviewActivity(context, textFieldState.value) },
+                        ) {
+                            Text("Открыть вторую Activity")
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun routeToTextReviewActivity(context: Context, text: String) {
+        val intent = Intent(context, MessageReviewActivity::class.java).apply {
+            putExtra("text", text)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTemplateProjectTheme {
-        Greeting("Android")
+        context.startActivity(intent)
     }
 }
