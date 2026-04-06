93b68989fee7d8a9a2eb1f94e20f14229e1d35b9
Shimchuk_A <117798841+A-Shimchuk@users.noreply.github.com>
2026-04-06 14:47:57 +0300

    Update ApplicationsApiService.kt
    Правки по МРу

--- app/src/main/java/com/example/androidtemplateproject/screens/appList/data/network/ApplicationsApiService.kt
+++ app/src/main/java/com/example/androidtemplateproject/screens/appList/data/network/ApplicationsApiService.kt
@@ -5,13 +5,13 @@ import retrofit2.http.Path
 
 interface ApplicationsApiService {
 
-    companion object {
-        const val BASE_URL = "http://185.103.109.134/"
-    }
-
     @GET("catalog")
     suspend fun getApplications(): List<NetworkApplicationDTO>
 
     @GET("catalog/{id}")
     suspend fun getApplicationDetails(@Path("id") id: String): NetworkApplicationDetailDTO
+
+    companion object {
+        const val BASE_URL = "http://185.103.109.134/"
+    }
 }
